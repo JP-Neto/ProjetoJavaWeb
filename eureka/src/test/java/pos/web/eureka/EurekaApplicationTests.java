d392ba1b9d4fd38dfc8363d18415712f8f8eb8d2
Jose Neto <113240030+JP-Neto@users.noreply.github.com>
2023-01-31 14:38:25 -0300

    Eureka commit

---
+++ eureka/src/test/java/pos/web/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package pos.web.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
